bc7cdb4cbde5bf8e151b7f6b4a8391bb42170d03
红纸 <paper@zhz.io>
2021-01-20 18:57:17 +0800

    update

---
+++ 5最长回文串/Example/Example/main.swift
@@ -0,0 +1,63 @@
+//
+//  main.swift
+//  Example
+//
+//  Created by 红纸 on 2020/10/26.
+//
+
+import Foundation
+//String扩展
+extension String {
+    //subscript函数可以检索数组中的值
+    //直接按照索引方式截取指定索引的字符
+    subscript (_ i: Int) -> Character {
+        //读取字符
+        get {return self[index(startIndex, offsetBy: i)]}
+
+        //修改字符
+        set
+        {
+            //转换为字符数组
+            var arr:[Character] = Array(self)
+            arr[i] = newValue
+            self = String(arr)
+        }
+    }
+
+    //String转ASCII整数值(定义小写为整数值)
+    public var ascii: Int {
+        return Int(unicodeScalars.first?.value ?? 0)
+    }
+}
+
+
+func longestPalindrome(_ s: String) -> String {
+    var start = 0
+    var end = 0
+    for i in 0..<s.count {
+        let len1 = expandAround(left: i, right: i, s: s)
+        let len2 = expandAround(left: i, right: i+1, s: s)
+        let len = max(len1, len2)
+        if len > (end - start) {
+            start = i - (len - 1) / 2;
+            end = i + len / 2;
+        }
+    }
+    return String(s[s.index(s.startIndex, offsetBy: start)...s.index(s.startIndex, offsetBy: end)])
+}
+
+
+func expandAround(left: Int, right: Int, s: String) -> Int {
+    var _l = left
+    var _r = right
+    while _l >= 0 && _r < s.count && s[_l] == s[_r]  {
+        _l -= 1
+        _r += 1
+    }
+    return _r - _l - 1
+}
+
+
+
+print(longestPalindrome("asddsadsfd"))
+print(longestPalindrome("babad"))
