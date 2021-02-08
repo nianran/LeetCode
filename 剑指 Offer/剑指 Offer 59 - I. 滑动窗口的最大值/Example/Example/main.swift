f240707b421080d097dcfc24b33d29f197e78ab5
红纸 <paper@zhz.io>
2021-02-08 13:57:55 +0800

    up

--- 剑指 Offer/剑指 Offer 59 - I. 滑动窗口的最大值/Example/Example/main.swift
+++ 剑指 Offer/剑指 Offer 59 - I. 滑动窗口的最大值/Example/Example/main.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
-    
+class Solution {
+    func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
+        var queue = [Int]()
+        var result = [Int]()
+        let length = nums.count
+        for i in 0..<length {
+            while (queue.count > 0 && nums[i] > nums[queue.last!]) {
+                queue.removeLast()
+            }
+            queue.append(i)
+
+            let w = i - k + 1
+            if w >= 0 {
+                if w > queue.first! {
+                    queue.removeFirst()
+                }
+                result.append(nums[queue.first!])
+            }
+        }
+        return result
+    }
 }
+
+print(maxSlidingWindow([1,3,-1,-3,5,3,6,7],3))
